7c1d210f07d94fdafab00fff31e4b0b8a05196b2
Maksym Skorchenko <m.skorchenko@gmail.com>
2022-06-08 15:50:37 +0300

    GROUPS

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -23,16 +23,16 @@ namespace SL.Scripts
         private float _energy;
         
         private Rigidbody2D _rigidbody;
-        private Vector3 _lastPosition;
+       // private Vector3 _lastPosition;
         
         private CellPhase _phase = CellPhase.Initiation;
-        private float _energyFine = 1.1f;
+        private float _energyFine = .4f;
         private float _dieEnergy = 10f;
         
         private float _coldDownTime = 0;
 
         private float _scaleMultiplier = 1f;
-        private float _energyPerUnit = 100f;
+        private float _energyPerUnit = 100;
 
         private float _timeToBirtMinValue = .1f; //for coloring, R channel
         private float _birthSizeMinValue = 80f; //for coloring, G channel
@@ -42,27 +42,34 @@ namespace SL.Scripts
         private float _birthSizeMaxValue = 120f; //for coloring, G channel
         private float _birthForceMaxValue = 40f; //for coloring, B channel
 
+        //private Vector3 prevPos;
+
+        public Action<Cell> OnDie;
+        
         private void Awake()
         {
             _rigidbody = GetComponentInParent<Rigidbody2D>();
         }
+
+        private List<Cell> children = new List<Cell>();
         
         void Start()
         {
-            _lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+           // _lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             transform.Rotate(Vector3.forward, Random.Range(0, 359));
         }
         
         void Update()
         {
             float scale = 1f;
+            
             if (_phase != CellPhase.Initiation)
             {
                 scale = (Mathf.Sqrt((_energy / _energyPerUnit) / Mathf.PI) * 2f)* _scaleMultiplier;
                 _energy -= _energyFine * (_energy / _energyPerUnit);
                 if (_energy < _dieEnergy)
                 {
-                    Destroy(gameObject);
+                    Kill();
                     return;
                 }
                 
@@ -96,47 +103,135 @@ namespace SL.Scripts
                 _body.color = new Color(r, g, b);
                 
                 _phase = CellPhase.Life;
+
+               // prevPos = transform.position;
                 return;
             }
             
             if (_phase.Equals(CellPhase.Life))
             {
-                _energy += GetLightVolume() * (_rigidbody.velocity.magnitude) * (_energy/_energyPerUnit);
+               // _energy += GetLightVolume() * (_rigidbody.velocity.magnitude);// * (_energy/_energyPerUnit);
+                //_energy += GetLightVolume() * (prevPos - transform.position).magnitude * 100;// * (_energy/_energyPerUnit);
+                _energy += GetLightVolume() * (_energy/_energyPerUnit);
+                //_energy += .1f;
                 //if ((_lastPosition - transform.position).magnitude == 0f)
-                if (_rigidbody.velocity.magnitude < 1f)
+                // if ((prevPos - transform.position).magnitude <= 0f)
+                // //if (_rigidbody.velocity.magnitude < 1f)
+                // {
+                //     _coldDownTime += Time.deltaTime;
+                //     if (_coldDownTime > _genome.TimeToBirth)
+                //     {
+                //         _phase = CellPhase.Dividing;
+                //         return;
+                //     }
+                // }
+
+                if ((int) _energy / (int) _genome.BirthSize > 2)
                 {
-                    _coldDownTime += Time.deltaTime;
-                    if (_coldDownTime > _genome.TimeToBirth)
+                    Divide();
+                    //return;
+                }
+
+                if (children.Count > 0)
+                {
+                    List<Cell> toKill = new List<Cell>();
+                    Vector3 pos = transform.position;
+                    children.ForEach(item =>
                     {
-                        _phase = CellPhase.Dividing;
-                        return;
+                        Vector3 cPos = item.transform.position;
+                        float mag = (cPos - pos).magnitude;
+
+                        if (mag > 2)
+                        {
+                            toKill.Add(item);
+                        }
+
+                        if (mag < 0.9 || mag > 1.1)
+                        {
+                            Vector3 dirToParent = (pos - cPos).normalized;
+                            float dMag = 1 - mag;
+                            dirToParent *= dMag > 0 ? -1 : 1;
+                            
+                            Vector3 newCPos = Vector3.Lerp(cPos, cPos + dirToParent, .25f);
+                            Vector3 newPos = Vector3.Lerp(pos, pos - dirToParent, .25f / children.Count);
+                            item.transform.position = newCPos;
+                            
+                            Debug.Log("Mag " + mag + " pos " + pos + " cpos " + cPos);
+                            Debug.Log("DirToP " + dirToParent + " dmag " + dMag);
+                            Debug.Log("newCPos " + newCPos);
+
+                            pos = newPos;
+                        }
+                    });
+
+                    transform.position = pos;
+
+                    foreach (var item in toKill)
+                    {
+                        item.Kill();
                     }
                 }
                 
-                _lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+               // _lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+                //prevPos = transform.position;
             }
 
-            if (_phase.Equals(CellPhase.Dividing))
-            {
-                Divide();
-                Destroy(gameObject);
-            }
+            // if (_phase.Equals(CellPhase.Dividing))
+            // {
+            //     Divide();
+            //     _phase = CellPhase.Life;
+            //     //Destroy(gameObject);
+            // }
         }
-        
+
+        public void Kill()
+        {
+            OnDie?.Invoke(this);
+            Destroy(gameObject);
+        }
 
         private void Divide()
         {
             float devidingEffectivity = (_genome.TimeToBirth * 7) / (_energy/_energyPerUnit);
             devidingEffectivity = devidingEffectivity > 1 ? 1 : devidingEffectivity;
-            _energy = _energy * devidingEffectivity;
+            //_energy = _energy * devidingEffectivity;
             
             int ChildrenQuantity = (int)_energy / (int)_genome.BirthSize;
 
-            for (int i = 0; i < ChildrenQuantity; i++)
+            if (ChildrenQuantity > 1)
             {
-                Cell cell = Instantiate(CellPrefab, transform.position, Quaternion.identity).GetComponent<Cell>();
-                cell.SetGenome(_genome.Clone().Mutate());
-                cell.SetEnergy(_energy/ChildrenQuantity);
+                for (int i = 0; i < ChildrenQuantity; i++)
+                {
+                    if ((int)_energy / (int)_genome.BirthSize <= 1)
+                        break;
+                    
+                    Cell cell = Instantiate(CellPrefab, transform.position, Quaternion.identity).GetComponent<Cell>();
+                    cell.SetGenome(_genome.Clone().Mutate());
+                    cell.SetEnergy(_energy / ChildrenQuantity);
+
+                    _energy -= _energy / ChildrenQuantity;
+                    cell.OnDie += OnDieHandler;
+                    
+                    children.Add(cell);
+                    
+
+                    // gameObject.AddComponent(typeof(SpringJoint2D));
+                    // SpringJoint2D joint = gameObject.GetComponent<SpringJoint2D>();
+                    // joint.distance = 2;
+                    // //joint.enableCollision = true;
+                    // //joint.dampingRatio = 1;
+                    // joint.autoConfigureDistance = false;
+                    // joint.connectedBody = cell._rigidbody;
+                }
+            }
+        }
+
+        private void OnDieHandler(Cell cell)
+        {
+            if (children.Contains(cell))
+            {
+                children.Remove(cell);
             }
         }
 
